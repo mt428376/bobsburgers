fd92f073262690d833a9f3e2ad9601f789b745dc
mya todd <mya.todd428376@gmail.com>
2021-01-26 23:30:58 -0500

    fixed other bug

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,26 @@
 var mysql = require("mysql");
 
-
-
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else {
+} else {
   connection = mysql.createConnection({
-    host: 'td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
-    user:'bmiwmx1rj9wzxa4s'
-    password: 'n5dz5k0flo2fzh1f'
-    database: 'g0ktoydr92mob75c'
-
-  })
-
-  }
+    host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "bmiwmx1rj9wzxa4s",
+    password: "n5dz5k0flo2fzh1f",
+    database: "g0ktoydr92mob75c",
+  });
+}
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Goldenrose1.",
-  database: "bobsburgers_db"
+  database: "bobsburgers_db",
 });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
